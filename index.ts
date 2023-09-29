0ba9995adc76e2787fffc1dbc018c022a20c789f
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-09-29 11:24:49 -0400

    incorporating time window

--- index.ts
+++ index.ts
@@ -14,9 +14,9 @@ interface ActuData {
 }
 
 async function getData(start: Date, end: Date): Promise<ActuData> {
-  // console.log("conn: starting mongo client");
   const uri = "mongodb://192.168.0.128:27017";
   const client = new MongoClient(uri);
+  console.log("conn: starting mongo client", uri, client);
   const db = client.db("actur");
   const articles = db.collection("articles");
   const ndocs = await articles.countDocuments();
@@ -60,22 +60,25 @@ async function getData(start: Date, end: Date): Promise<ActuData> {
 }
 
 const server = Bun.serve({
-  port: 3433, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
+  port: 33433, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
   hostname: "0.0.0.0", // defaults to "0.0.0.0"
 
   fetch(req) {
-    // console.log("request", req);
+    console.log("request", req);
     const url = new URL(req.url);
     const sparams = url.searchParams;
     // console.log("timeframe", sparams);
-    let timeframe = sparams.get("timeframe") || "0";
+    const timeframe = sparams.get("timeframe") || "0";
+    const timewindow = sparams.get("timewindow") || "2";
 
     // // db setup
-    const timewindow = 2;
+    // const timewindow = 2;
     const tf = parseInt(timeframe, 10);
+    const tw = parseInt(timewindow, 10);
+    console.log("params", tf, tw);
     const now = new Date();
-    const end: Date = subHours(now, tf * timewindow);
-    const start: Date = subHours(end, timewindow);
+    const end: Date = subHours(now, tf * tw);
+    const start: Date = subHours(end, tw);
     // console.log("start/end", start, end);
     const reply = getData(start, end).then((res) => Response.json(res));
 
